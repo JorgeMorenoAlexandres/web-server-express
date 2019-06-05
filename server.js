f243a8b9fccb5b7fd6fc5c569d29704f5cd7df8a
George <jorgemorenoalexandres@gmail.com>
2019-06-04 22:55:41 -0500

    Primer comentario

---
+++ server.js
@@ -0,0 +1,32 @@
+//librería express para servicios web 
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+//creación del middlewere
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs')
+
+
+
+//URL redireccionamiento
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'jorge'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {});
+});
+
+app.listen(port, () => {
+    console.log(`El puerto escuchado es ${port}`);
+});
